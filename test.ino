a972a60c4ff6ca7fb006339211f4947e7eda017f
Alexander Liman <liman324@yandex.ru>
2020-01-07 20:09:16 +0600

    Create test.ino

---
+++ test.ino
@@ -0,0 +1,24 @@
+#include <Wire.h>
+#include <PT2258.h>
+
+PT2258 pt;
+
+void setup() {
+ audio();
+
+}
+
+void loop() {
+
+}
+
+void audio(){
+        pt.setVolume(0); //  int 0...79     
+        pt.setVol1(0); //  int 0...79       
+        pt.setVol2(0); //  int 0...79 
+        pt.setVol3(0); //  int 0...79 
+        pt.setVol4(0); //  int 0...79 
+        pt.setVol5(0); //  int 0...79 
+        pt.setVol6(0); //  int 0...79 
+        pt.setMute(0); // mute 0 - off / 1 - on
+  }
