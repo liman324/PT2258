0f2c7a9e4408b87ba9c43f2584b878d6d2eb05a1
Alexander Liman <liman324@yandex.ru>
2020-05-22 08:04:01 +0600

    Update PT2258.cpp

--- PT2258.cpp
+++ PT2258.cpp
@@ -16,62 +16,57 @@ void PT2258::setVolume(int vol){
   vol = 79 - vol;
   int vol10 = vol/10;
   int vol1 = vol%10;
-  writeWire(vol10+0b11010000, vol1+ 0b11100000);
+  writeWire(vol10 + VOL_MASTER_10, vol1 + VOL_MASTER_1);
 }
 
 void PT2258::setVol1(int v1){
   v1 = 79 - v1;
   int v1_10 = v1/10;
   int v1_1 = v1%10;
-  writeWire(v1_10+0b10000000, v1_1+ 0b10010000);
+  writeWire(v1_10 + VOL_1CH_10, v1_1 + VOL_1CH_1);
 }
 
 void PT2258::setVol2(int v2){
   v2 = 79 - v2;
   int v2_10 = v2/10;
   int v2_1 = v2%10;
-  writeWire(v2_10+0b01000000, v2_1+ 0b01010000);
+  writeWire(v2_10 + VOL_2CH_10, v2_1 + VOL_2CH_1);
 }
 
 void PT2258::setVol3(int v3){
   v3 = 79 - v3;
   int v3_10 = v3/10;
   int v3_1 = v3%10;
-  writeWire(v3_10+0b00000000, v3_1+ 0b00010000);
+  writeWire(v3_10 + VOL_3CH_10, v3_1 + VOL_3CH_1);
 }
 
 void PT2258::setVol4(int v4){
   v4 = 79 - v4;
   int v4_10 = v4/10;
   int v4_1 = v4%10;
-  writeWire(v4_10+0b00100000, v4_1+ 0b00110000);
+  writeWire(v4_10 + VOL_4CH_10, v4_1 + VOL_4CH_1);
 }
 
 void PT2258::setVol5(int v5){
   v5 = 79 - v5;
   int v5_10 = v5/10;
   int v5_1 = v5%10;
-  writeWire(v5_10+0b01100000, v5_1+ 0b01110000);
+  writeWire(v5_10 + VOL_5CH_10, v5_1 + VOL_5CH_1);
 }
 
 void PT2258::setVol6(int v6){
   v6 = 79 - v6;
   int v6_10 = v6/10;
   int v6_1 = v6%10;
-  writeWire(v6_10+0b10100000, v6_1+ 0b10110000);
+  writeWire(v6_10 + VOL_6CH_10, v6_1 + VOL_6CH_1);
 }
 
 void PT2258::setMute(int mute){
-  if(mute==0){
   Wire.beginTransmission(PT2258_address);
-  Wire.write (0b11111000);
-  Wire.endTransmission();}
-  
-  if(mute==1){
-  Wire.beginTransmission(PT2258_address);
-  Wire.write (0b11111001);
-  Wire.endTransmission();}
+  Wire.write (MUTE + mute);
+  Wire.endTransmission();
 }
+  
 
 void PT2258::writeWire(char a, char b){
   Wire.beginTransmission(PT2258_address);
