e0a5f9282178399fe8abd5b51d08ef0731dc5cf3
Alexander Liman <liman324@yandex.ru>
2020-05-22 07:44:11 +0600

    Update test.ino

--- test.ino
+++ test.ino
@@ -4,8 +4,8 @@
 PT2258 pt;
 
 void setup() {
+ pt.clear(); 
  audio();
-
 }
 
 void loop() {
@@ -13,12 +13,12 @@ void loop() {
 }
 
 void audio(){
-        pt.setVolume(0); //  int 0...79     
-        pt.setVol1(0); //  int 0...79       
-        pt.setVol2(0); //  int 0...79 
-        pt.setVol3(0); //  int 0...79 
-        pt.setVol4(0); //  int 0...79 
-        pt.setVol5(0); //  int 0...79 
-        pt.setVol6(0); //  int 0...79 
-        pt.setMute(0); // mute 0 - off / 1 - on
+        pt.setVolume(79); //  int 0...79     
+        //pt.setVol1(0); //  int 0...79       
+        //pt.setVol2(0); //  int 0...79 
+        //pt.setVol3(0); //  int 0...79 
+        //pt.setVol4(0); //  int 0...79 
+        //pt.setVol5(0); //  int 0...79 
+        //pt.setVol6(0); //  int 0...79 
+        //pt.setMute(0); // mute 0 - off / 1 - on
   }
